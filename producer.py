c8c58a6732a4da1f7567912dc2baf44f754f273f
Jelena Lucic <lucicjelena94@gmail.com>
2022-09-07 20:06:48 +0200

    Add comment regarding .gz version of JSON file

--- producer.py
+++ producer.py
@@ -6,6 +6,10 @@ if __name__ == '__main__':
         bootstrap_servers='localhost:9092'
     )
 
+    # Uncomment the following line of code if you're using stream.jsonl.gz instead of unzipped version
+    # and comment out the line after that
+    # with gzip.open('./stream.jsonl.gz', 'rb') as json_file:
+    
     json_file = open('./stream.jsonl', 'rb')
     for line in json_file:
         producer.send('messages', line)
